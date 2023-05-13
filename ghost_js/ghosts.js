4d6f01ba0867ac4e2d2532a18ef0e3a2abe5cfa7
Guillermo <jsalinas@europeanyouthmedia.com>
2023-05-13 20:05:32 +0200

    Se termina la modificación de la inteligencia Artificial ahora va y optimizado 10 horas de modificación

--- ghost_js/ghosts.js
+++ ghost_js/ghosts.js
@@ -9,8 +9,9 @@ ghostRed.positionx = 280;
 ghostRed.positiony = 250;
 ghostRed.tamanox = 20;
 ghostRed.tamanoy = 20;
-ghostRed.pospx = 0;
-ghostRed.pospy = 0;
+ghostRed.pospx = 280;
+ghostRed.pospy = 250;
+ghostRed.direccion = "DERECHA";
 ghostRed.src = "../ghost_image/ghost_red.svg"
 ghostRed.id = "gred";
 ghostRed.onload = function () {
@@ -29,8 +30,9 @@ ghostPink.positionx = 300;
 ghostPink.positiony = 250;
 ghostPink.tamanox = 20;
 ghostPink.tamanoy = 20;
-ghostPink.pospx = 0;
-ghostPink.pospy = 0;
+ghostPink.pospx = 300;
+ghostPink.pospy = 250;
+ghostPink.direccion = "DERECHA";
 ghostPink.id = "gpink";
 ghostPink.src = "../ghost_image/ghost_pink.svg"
 ghostPink.onload = () => contextcanvas.drawImage(ghostPink, ghostPink.positionx, ghostPink.positiony);
@@ -45,9 +47,9 @@ ghostSky.positionx = 320;
 ghostSky.positiony = 250;
 ghostSky.tamanox = 20;
 ghostSky.tamanoy = 20;
-ghostSky.pospx = 0;
-ghostSky.pospy = 0;
-
+ghostSky.pospx = 320;
+ghostSky.pospy = 250;
+ghostSky.direccion = "DERECHA";
 ghostSky.src = "../ghost_image/ghost_sky.svg"
 ghostSky.id = "gsky";
 ghostSky.onload = () => contextcanvas.drawImage(ghostSky, ghostSky.positionx, ghostSky.positiony);
@@ -63,8 +65,9 @@ ghostYellow.positionx = 260;
 ghostYellow.positiony = 250;
 ghostYellow.tamanox = 20;
 ghostYellow.tamanoy = 20;
-ghostYellow.pospx = 0;
-ghostYellow.pospy = 0;
+ghostYellow.pospx = 260;
+ghostYellow.pospy = 250;
+ghostYellow.direccion = "DERECHA";
 ghostYellow.src = "../ghost_image/ghost_Yellow.svg"
 ghostYellow.id = "gyellow";
 ghostYellow.onload = () => contextcanvas.drawImage(ghostYellow, ghostYellow.positionx, ghostYellow.positiony);
@@ -75,35 +78,10 @@ ghostYellow.ia = () => iaFantasmal(setInterval, ghostYellow);
 //función de la inteligencia artificial de los fantasma, fue separada ya que va ver muchos fantasmas para ahorrar lineas de código adicional
 function iaFantasmal(callback, ghost) {
     callback(function () {
-        pacmanlugarx = pacman.positionx;
-        pacmanlugary = pacman.positiony;
-        // aquí obtengo números aleatorios entre -1 y 1 para que se mueva el fantasma
-        ghost.pospx = calcularDistanciaX(ghost, pacmanlugarx); // esto genera un número aleatorio entre -1 y 1
-        ghost.pospy = calcularDistanciaY(ghost, pacmanlugary); // esto genera un número aleatorio entre -1 y 1
-        seleccionMovimientoGhostx(ghost,1);
-        if (seleccionMovimientoGhostx(ghost)) {
-            ghost.pospy = pospy;
-            ghost.pospx = 0;
-        } else {
-            if (seleccionMovimientoGhosty(ghost)) {
-                ghost.pospx = -pospx
-                ghost.positiony = 0;
-            }
-        }
-        // aqui miramos si que ha presionado el fantasma
-
-        if (ghost.pospx > 0) {
-            ghost.positionx += velocidadFantasma;
-        } else if (ghost.pospx < 0) {
-            ghost.positionx -= velocidadFantasma;
-        }
-
-        if (ghost.pospy > 0) {
-            ghost.positiony += velocidadFantasma;
-        } else if (ghost.pospy < 0) {
-            ghost.positiony -= velocidadFantasma;
-        }
+        //función para que se mueva el fantasma
         
+        movimientoFantasma(ghost)
+
         if (ghost.positionx > 600) {
             // Si hay colisión, mover al jugador de vuelta
             if (ghost.pospx > 0) {
@@ -118,119 +96,150 @@ function iaFantasmal(callback, ghost) {
         }
     }, 110)
 }
-function seleccionMovimientoGhostx( ghost,xory) {
+
+
+//para que se mueva el fantasma:
+function movimientoFantasma(ghost) {
+    if(ghost.positiony == pacman.positiony){
+        if (pacman.positionx > ghost.positionx) {
+            ghost.direccion = "DERECHA";
+        }else{
+            ghost.direccion = "IZQUIERDA";
+        }
+    }
+    if(ghost.positionx == pacman.positionx){
+        if (pacman.positiony > ghost.positiony) {
+            ghost.direccion = "ABAJO";
+        }else{
+            ghost.direccion = "ARRIBA";
+        }
+    }
+    
+    switch (ghost.direccion) {
+        case "DERECHA":
+            ghost.pospx += velocidadFantasma;
+            break;
+        case "IZQUIERDA":
+            ghost.pospx -= velocidadFantasma;
+        break;
+        case "ARRIBA":
+            ghost.pospy -= velocidadFantasma;
+        break;
+        case "ABAJO":
+            ghost.pospy += velocidadFantasma;
+        break;
+        default:
+            break;
+    }
     if (deteccionColision(ghost)||deteccionColisionFantasmas(ghost)) {
-        if (xory==1) {
-            xory=1;
-            ghost.pospy = ghost.pospy-1;
-            seleccionMovimientoGhostx(ghost,xory)
-        } else if(xory ==2){
-            xory=2;
-            ghost.pospx = ghost.pospx-1;
-            seleccionMovimientoGhostx(ghost,xory)
-        }
+        switch (ghost.direccion) {
+            case "DERECHA":
+                ghost.pospx -= velocidadFantasma;
+                ghost.direccion = "IZQUIERDA";
+                break;
+            case "IZQUIERDA":
+                ghost.pospx += velocidadFantasma;
+                ghost.direccion= "ARRIBA";
+            break;
+            case "ARRIBA":
+                ghost.pospy += velocidadFantasma;
+                ghost.direccion = "ABAJO";
+            break;
+            case "ABAJO":
+                ghost.pospy -= velocidadFantasma;
+                ghost.direccion = "DERECHA";
+            break;
+            default:
+                break;
+           }
+           
     } else {
-        if (ghost.pospx==0 && ghost.pospy == 0) {
-            if (pacman.positiony==ghost.pospy&&pacman.positionx==ghsot.pospx) {
-                return
-            } else {
-                xory =2
-                ghost.pospx = ghost.pospx+1
-                seleccionMovimientoGhostx(ghost,xory)
-            }
-        } else {
-            return
-        }
+        switch (ghost.direccion) {
+            case "DERECHA":
+                ghost.positionx = ghost.pospx;
+                break;
+            case "IZQUIERDA":
+                ghost.positionx = ghost.pospx;
+            break;
+            case "ARRIBA":
+                ghost.positiony = ghost.pospy;
+            break;
+            case "ABAJO":
+                ghost.positiony = ghost.pospy;
+            break;
+            default:
+                break;
+           }
+           return;
     }
+
+
+
+
 }
 
 
 
-//Empleo recursividad para saber donde ir el fantasma y que tome una decición según el camino que tenga
-function seleccionMovimientoGhosty(ghost) {
-    if (deteccionColisionFantasmas(ghost) || deteccionColision(ghost)) {
-        if (ghost.pospy > 0) {
-            ghost.pospy = -1;
-        } else if (ghost.pospy < 0) {
-            ghost.pospy = +1;
-        }
-        
-    }
-}
 
 //para saber si hubo una colisión con algún fantasma;
 function deteccionColisionFantasmas(ghost) {
     fantasmas = crearArrayFantasma();
-    avanceadicionalx = 0;
-    avanceadicionaly = 0
-    if (ghost.pospx > 0) {
-        avanceadicionalx += velocidadFantasma;
-    } else if (ghost.pospx < 0) {
-        avanceadicionalx -= velocidadFantasma;
-    }
-
-    if (ghost.pospy > 0) {
-        avanceadicionaly += velocidadFantasma
-    } else if (ghost.pospy < 0) {
-        avanceadicionaly -= velocidadFantasma
-    }
+    numColision = 0
+    colision =false;
+    
     for (let i = 0; i < fantasmas.length; i++) {
         if (ghost.id != fantasmas[i].id) {
 
-            if (ghost.positionx + avanceadicionalx < fantasmas[i].positionx + fantasmas[i].tamanox &&
-                (ghost.positionx + avanceadicionalx) + ghost.tamanox > fantasmas[i].positionx &&
-                ghost.positiony + avanceadicionaly < fantasmas[i].positiony + fantasmas[i].tamanoy &&
-                (ghost.positiony + avanceadicionaly) + ghost.tamanoy > fantasmas[i].positiony) {
+            if (ghost.pospx < fantasmas[i].pospx + fantasmas[i].tamanox &&
+                ghost.pospx + ghost.tamanox > fantasmas[i].pospx &&
+                ghost.pospy < fantasmas[i].pospy + fantasmas[i].tamanoy &&
+                ghost.pospy + ghost.tamanoy > fantasmas[i].pospy) {
                 // Si hay colisión, mover al jugador de vuelta
-
-                return true;
-
-            } else {
-                return false;
-            }
+               
+                numColision += 1;
+            } 
         }
 
     }
+    if (numColision>0) {
+        colision = true;
+    } else {
+        colision = false;
+    }
+    return colision;
 }
 //detecta si hubo una colisión con algún muro
 function deteccionColision(ghost) {
-    fantasmas = crearArrayFantasma();
-    avanceadicionalx = 0;
-    avanceadicionaly = 0
-    if (ghost.pospx > 0) {
-        avanceadicionalx += velocidadFantasma;
-    } else if (ghost.pospx < 0) {
-        avanceadicionalx -= velocidadFantasma;
-    }
-
-    if (ghost.pospy > 0) {
-        avanceadicionaly += velocidadFantasma
-    } else if (ghost.pospy < 0) {
-        avanceadicionaly -= velocidadFantasma
-    }
+    colision = false;
+    numColision = 0;
     // Detectar colisiones con los muros
     for (var i = 0; i < laberinto1.length; i++) {
-        if (ghost.positionx + avanceadicionalx < laberinto1[i].x + laberinto1[i].width &&
-            (ghost.positionx + avanceadicionalx) + ghost.tamanox > laberinto1[i].x &&
-            ghost.positiony + avanceadicionaly < laberinto1[i].y + laberinto1[i].height &&
-            (ghost.tamanoy + avanceadicionaly) + ghost.positiony > laberinto1[i].y) {
-
-            return true;
-
-        } else return false;
+        if (ghost.pospx < laberinto1[i].x + laberinto1[i].width &&
+            ghost.pospx + ghost.tamanoy > laberinto1[i].x &&
+            ghost.pospy < laberinto1[i].y + laberinto1[i].height &&
+            ghost.tamanoy + ghost.pospy > laberinto1[i].y) {
+            
+            numColision +=1;
+        } else {
+            if (ghost.positionx > 600) {
+                // Si hay colisión, mover al jugador de vuelta
+                if (event.keyCode === 39) {
+                    ghost.positionx = 0;
+                }
+            }
+            if (ghost.positionx < 0) {
+                // Si hay colisión, mover al jugador de vuelta
+                if (event.keyCode === 37) {
+                    ghost.positionx = 590;
+                }
+            }
+            
+        }
     }
-}
-
-function calcularDistanciaX(ghost, pacmanx) {
-    //para calcular la distancia del pacman para saber si se mueve para la izquierda o derecha vamos a implementar 
-    //un calculo matematico sencillo una resta pero 
-    //una que se aprecia el orden de objetos que esten este avanzará a la izquierda cuando la resta se positiva.
-    //y este avanzará a la izquierda cuando la resta se negativa
-    return pacmanx - ghost.positionx;
-}
-
-function calcularDistanciaY(ghost, pacmany) {
-    //está es lo mismo que la función de arriba calcula según la resta
-    //esto va avanzará abajo cuando sea positivo entonces el pacman siempre va ser mayor
-    return pacmany - ghost.positiony;
+    if (numColision>0) {
+        colision = true;
+    } else {
+        colision =false;
+    }
+    return colision;
 }
